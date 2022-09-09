4d1ab80fff5686d2164305d7e18a383eb5490dbe
Jack McNamara <jack.mcnamara@uq.edu.au>
2022-09-09 15:49:49 +1000

    good

--- franka_example_controllers/src/joint_position_example_controller.cpp
+++ franka_example_controllers/src/joint_position_example_controller.cpp
@@ -10,9 +10,14 @@
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
 
+/*
+Joint Position Controller used for testing IMU Sensors. Joints 4, 5 and 6 are individually controlled 
+with a frequency varying sine wave to create a vibratory motion in 3 axes.
+*/
+
 namespace franka_example_controllers {
 
-bool JointPositionExampleController::init(hardware_interface::RobotHW* robot_hardware,
+bool JointPositionExampleController::init(hardware_interface::RobotHW* robot_hardware, // Initialisation of controller
                                           ros::NodeHandle& node_handle) {
   position_joint_interface_ = robot_hardware->get<hardware_interface::PositionJointInterface>();
   if (position_joint_interface_ == nullptr) {
@@ -40,9 +45,9 @@ bool JointPositionExampleController::init(hardware_interface::RobotHW* robot_har
     }
   }
 
-  std::array<double, 7> q_start{{0.0, 0.0, 0, -M_PI_2, 0.0, M_PI_2, M_PI_4}};
+  std::array<double, 7> q_start{{0.0, 0.0, 0, -M_PI_2, 0.0, M_PI_2, M_PI_4}}; // Testing for starting pose of the robot to ensure it will not move excessively
   for (size_t i = 0; i < q_start.size(); i++) {
-    if (std::abs(position_joint_handles_[i].getPosition() - q_start[i]) > 0.3) {
+    if (std::abs(position_joint_handles_[i].getPosition() - q_start[i]) > 0.3) { // 0.3 was originally 0.1, changed as the franka_ros move to start function does not work (need to program one myself)
       ROS_ERROR_STREAM(
           "JointPositionExampleController: Robot is not in the expected starting position for "
           "running this example. Run `roslaunch franka_example_controllers move_to_start.launch "
@@ -54,47 +59,26 @@ bool JointPositionExampleController::init(hardware_interface::RobotHW* robot_har
   return true;
 }
 
-void JointPositionExampleController::starting(const ros::Time& /* time */) {
+void JointPositionExampleController::starting(const ros::Time& /* time */) { // Controller start, receives and stores initial position of the robot
   for (size_t i = 0; i < 7; ++i) {
     initial_pose_[i] = position_joint_handles_[i].getPosition();
   }
-  std::array<double, 7> end_pose_ = {0.0, 0.0, 0, -M_PI_2, 0.0, M_PI_2, M_PI_4};
-  elapsed_time_ = ros::Duration(0.0);
+  std::array<double, 7> end_pose_ = {0.0, 0.0, 0, -M_PI_2, 0.0, M_PI_2, M_PI_4}; // not needed or used, can be used as joint positions to achieve a 90 bend in robot
+  elapsed_time_ = ros::Duration(0.0); // Sets timer to be used to 0
 }
 
-void JointPositionExampleController::update(const ros::Time& /*time*/,
+void JointPositionExampleController::update(const ros::Time& /*time*/, // Update function to loop continuously to loop through
                                             const ros::Duration& period) {
-  elapsed_time_ += period;
-  ros::Duration duration = ros::Duration(42.6*3+0.5*60);
-  // ROS_ERROR("Elapsed Time: %f", elapsed_time_.toSec());
-  double T = 10.0;
+  elapsed_time_ += period; // Add the time to update with the elapsed time, acts as a timer
+  ros::Duration duration = ros::Duration(42.6*3+5*60); // Value set for the duration of the test to reset the timer after the 3 axis movements
 
-  double delta_angle = M_PI / 48 * (std::sin(4*M_PI  * elapsed_time_.toSec())) * 0.2;
-  // double omega = 0.6*M_PI + (6*M_PI-0.6*M_PI)  * (elapsed_time_.toSec()/10);
-  // double delta_angle_var = M_PI / 48 * (std::sin(omega * elapsed_time_.toSec())) * 0.3;
+  double T = 10.0; // unused
 
-  // ROS_ERROR("Time: %f, Delta Angle: %f, Omega: %f", elapsed_time_.toSec(), delta_angle_var, omega);
+  double delta_angle = M_PI / 48 * (std::sin(4*M_PI  * elapsed_time_.toSec())) * 0.2; // unused
 
-  // double omega_1 = 0.6*M_PI + (6*M_PI-0.6*M_PI)  * (elapsed_time_.toSec()/10);
-  // double delta_angle_var_1 = M_PI / 48 * (std::sin(omega_1 * elapsed_time_.toSec())) * 0.2;
-
-  // double omega_2 = 0.6*M_PI + (6*M_PI-0.6*M_PI)  * ((elapsed_time_.toSec()-1.5*T)/10);
-  // double delta_angle_var_2 = M_PI / 48 * (std::sin(omega_2 * (elapsed_time_.toSec()-1.5*T))) * 0.2;
-
-  // double omega_3 = 0.6*M_PI + (6*M_PI-0.6*M_PI)  * ((elapsed_time_.toSec()-3*T)/10);
-  // double delta_angle_var_3 = M_PI / 48 * (std::sin(omega_3 * (elapsed_time_.toSec()-3*T))) * 0.2;
-
-  // if (elapsed_time_.toSec() <= T){
-  //   ROS_ERROR("Time: %f, Delta Angle: %f, Omega: %f", elapsed_time_.toSec(), delta_angle_var_1, omega_1);
-  //   for (size_t i = 0; i < 7; ++i) {
-  //     if (i == 4) {
-  //       position_joint_handles_[i].setCommand(initial_pose_[i] + delta_angle_var_1);
-  //     } else {
-  //       position_joint_handles_[i].setCommand(initial_pose_[i]);
-  //     }
-  //   }
-  // }
-
+  /*
+  
+  */
   double w_1 = 3 * M_PI / 180 * (std::sin(0.20 * 2 * M_PI * (elapsed_time_.toSec())));
   double w_2 = 3 * M_PI / 180 * (std::sin(0.50 * 2 * M_PI * (-15.0 + elapsed_time_.toSec())));
   double w_3 = 3 * M_PI / 180 * (std::sin(1.00 * 2 * M_PI * (-23.0 + elapsed_time_.toSec())));
@@ -118,7 +102,7 @@ void JointPositionExampleController::update(const ros::Time& /*time*/,
   double w_5_3 = 3 * M_PI / 180 * (std::sin(2.00 * 2 * M_PI * (-32.8 - 42.6 - 42.6 + elapsed_time_.toSec())));
   double w_6_3 = 3 * M_PI / 180 * (std::sin(2.50 * 2 * M_PI * (-37.8 - 42.6 - 42.6 + elapsed_time_.toSec())));
   double w_7_3 = 3 * M_PI / 180 * (std::sin(3.20 * 2 * M_PI * (-42.6 - 42.6 - 42.6 + elapsed_time_.toSec())));
-
+  
   if (elapsed_time_.toSec() <= 15){
     ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_1);
     for (size_t i = 0; i < 7; ++i) {
@@ -281,24 +265,14 @@ void JointPositionExampleController::update(const ros::Time& /*time*/,
         position_joint_handles_[i].setCommand(initial_pose_[i]);
       }
     }
-  }
-  // } else if (elapsed_time_.toSec() <= 47.6){
-  //   ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_7);
-  //   for (size_t i = 0; i < 7; ++i) {
-  //     if (i == 4) {
-  //       position_joint_handles_[i].setCommand(initial_pose_[i] + w_7);
-  //     } else {
-  //       position_joint_handles_[i].setCommand(initial_pose_[i]);
-  //     }
-  //   }
-  else if (elapsed_time_.toSec() < (42.6*3+0.5*60))
+  } else if (elapsed_time_.toSec() < (42.6*3+5*60))
   {
     ROS_ERROR("Time: %f", elapsed_time_.toSec());
     for (size_t i = 0; i < 7; ++i) {
         position_joint_handles_[i].setCommand(initial_pose_[i]);
     }
   }
-  else if (elapsed_time_.toSec() == (42.6*3+0.5*60))
+  else if (elapsed_time_.toSec() == (42.6*3+5*60))
   {
     ROS_ERROR("Time: %f", elapsed_time_.toSec());
     for (size_t i = 0; i < 7; ++i) {
@@ -306,58 +280,6 @@ void JointPositionExampleController::update(const ros::Time& /*time*/,
     }
     elapsed_time_ = elapsed_time_ - duration;
   }
-
-  // else if (elapsed_time_.toSec() <= 1.5*T && elapsed_time_.toSec() > T){
-  //   ROS_ERROR("Time: %f", elapsed_time_.toSec());
-  //   for (size_t i = 0; i < 7; ++i){
-  //       position_joint_handles_[i].setCommand(initial_pose_[i]);
-  //     }
-  // }
-  // else if (elapsed_time_.toSec() <= 2.5*T)
-  // {
-    
-  //   ROS_ERROR("Time: %f, Delta Angle: %f, Omega: %f", elapsed_time_.toSec(), delta_angle_var_2, omega_2);
-  //   for (size_t i = 0; i < 7; ++i) {
-  //     if (i == 5) {
-  //       position_joint_handles_[i].setCommand(initial_pose_[i] + delta_angle_var_2);
-  //     } else {
-  //       position_joint_handles_[i].setCommand(initial_pose_[i]);
-  //     }
-  //   }
-  // }
-  // else if (elapsed_time_.toSec() <= 3*T)
-  // {
-  //   ROS_ERROR("Time: %f", elapsed_time_.toSec());
-  //   for (size_t i = 0; i < 7; ++i) {
-  //       position_joint_handles_[i].setCommand(initial_pose_[i]);
-  //     }
-  // }
-  // else if (elapsed_time_.toSec() <= 4*T)
-  // {  pub = rospy.Publisher("/equilibrium_pose", PoseStamped, queue_size=1)
-  //   ROS_ERROR("Time: %f, Delta Angle: %f, Omega: %f", elapsed_time_.toSec(), delta_angle_var_3, omega_3);
-  //   for (size_t i = 0; i < 7; ++i) {
-  //     if (i == 6) {
-  //       position_joint_handles_[i].setCommand(initial_pose_[i] + delta_angle_var_3);
-  //     } else {
-  //       position_joint_handles_[i].setCommand(initial_pose_[i]);
-  //     }
-  //   }
-  // }
-  // else if (elapsed_time_.toSec() < (5*T+6*T))
-  // {
-  //   ROS_ERROR("Time: %f", elapsed_time_.toSec());
-  //   for (size_t i = 0; i < 7; ++i) {
-  //       position_joint_handles_[i].setCommand(initial_pose_[i]);
-  //   }
-  // }
-  // else if (elapsed_time_.toSec() == (5*T+6*T))
-  // {
-  //   ROS_ERROR("Time: %f", elapsed_time_.toSec());
-  //   for (size_t i = 0; i < 7; ++i) {
-  //       position_joint_handles_[i].setCommand(initial_pose_[i]);
-  //   }
-  //   elapsed_time_ = elapsed_time_ - duration;
-  // }
   
 }
 
