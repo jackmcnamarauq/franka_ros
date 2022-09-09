0abf0f47df18fad3ef7be4d0e00cbd61895b17f6
Jack McNamara <jack.mcnamara@uq.edu.au>
2022-09-09 16:54:21 +1000

    end of day backup

--- franka_example_controllers/src/joint_position_example_controller.cpp
+++ franka_example_controllers/src/joint_position_example_controller.cpp
@@ -71,6 +71,7 @@ void JointPositionExampleController::update(const ros::Time& /*time*/, // Update
                                             const ros::Duration& period) {
   elapsed_time_ += period; // Add the time to update with the elapsed time, acts as a timer
   ros::Duration duration = ros::Duration(42.6*3+5*60); // Value set for the duration of the test to reset the timer after the 3 axis movements
+  // ros::Duration duration = ros::Duration(42.6*3+10); // Value set for the duration of the test to reset the timer after the 3 axis movements
 
   double T = 10.0; // unused
 
@@ -79,31 +80,82 @@ void JointPositionExampleController::update(const ros::Time& /*time*/, // Update
   /*
   
   */
-  double w_1 = 3 * M_PI / 180 * (std::sin(0.20 * 2 * M_PI * (elapsed_time_.toSec())));
-  double w_2 = 3 * M_PI / 180 * (std::sin(0.50 * 2 * M_PI * (-15.0 + elapsed_time_.toSec())));
-  double w_3 = 3 * M_PI / 180 * (std::sin(1.00 * 2 * M_PI * (-23.0 + elapsed_time_.toSec())));
-  double w_4 = 3 * M_PI / 180 * (std::sin(1.25 * 2 * M_PI * (-28.0 + elapsed_time_.toSec())));
-  double w_5 = 3 * M_PI / 180 * (std::sin(2.00 * 2 * M_PI * (-32.8 + elapsed_time_.toSec())));
-  double w_6 = 3 * M_PI / 180 * (std::sin(2.50 * 2 * M_PI * (-37.8 + elapsed_time_.toSec())));
-  double w_7 = 3 * M_PI / 180 * (std::sin(3.20 * 2 * M_PI * (-42.6 + elapsed_time_.toSec())));
-
-  double w_1_2 = 3 * M_PI / 180 * (std::sin(0.20 * 2 * M_PI * (elapsed_time_.toSec())));
-  double w_2_2 = 3 * M_PI / 180 * (std::sin(0.50 * 2 * M_PI * (-15.0 - 42.6 + elapsed_time_.toSec())));
-  double w_3_2 = 3 * M_PI / 180 * (std::sin(1.00 * 2 * M_PI * (-23.0 - 42.6 + elapsed_time_.toSec())));
-  double w_4_2 = 3 * M_PI / 180 * (std::sin(1.25 * 2 * M_PI * (-28.0 - 42.6 + elapsed_time_.toSec())));
-  double w_5_2 = 3 * M_PI / 180 * (std::sin(2.00 * 2 * M_PI * (-32.8 - 42.6 + elapsed_time_.toSec())));
-  double w_6_2 = 3 * M_PI / 180 * (std::sin(2.50 * 2 * M_PI * (-37.8 - 42.6 + elapsed_time_.toSec())));
-  double w_7_2 = 3 * M_PI / 180 * (std::sin(3.20 * 2 * M_PI * (-42.6 - 42.6 + elapsed_time_.toSec())));
-
-  double w_1_3 = 3 * M_PI / 180 * (std::sin(0.20 * 2 * M_PI * (elapsed_time_.toSec())));
-  double w_2_3 = 3 * M_PI / 180 * (std::sin(0.50 * 2 * M_PI * (-15.0 - 42.6 - 42.6 + elapsed_time_.toSec())));
-  double w_3_3 = 3 * M_PI / 180 * (std::sin(1.00 * 2 * M_PI * (-23.0 - 42.6 - 42.6 + elapsed_time_.toSec())));
-  double w_4_3 = 3 * M_PI / 180 * (std::sin(1.25 * 2 * M_PI * (-28.0 - 42.6 - 42.6 + elapsed_time_.toSec())));
-  double w_5_3 = 3 * M_PI / 180 * (std::sin(2.00 * 2 * M_PI * (-32.8 - 42.6 - 42.6 + elapsed_time_.toSec())));
-  double w_6_3 = 3 * M_PI / 180 * (std::sin(2.50 * 2 * M_PI * (-37.8 - 42.6 - 42.6 + elapsed_time_.toSec())));
-  double w_7_3 = 3 * M_PI / 180 * (std::sin(3.20 * 2 * M_PI * (-42.6 - 42.6 - 42.6 + elapsed_time_.toSec())));
+  double amplitude = 3 * M_PI / 180;
   
+  double w_1 = amplitude * (std::sin(0.20 * 2 * M_PI * (elapsed_time_.toSec())));
+  double w_2 = amplitude * (std::sin(0.50 * 2 * M_PI * (-15.0 + elapsed_time_.toSec())));
+  double w_3 = amplitude * (std::sin(1.00 * 2 * M_PI * (-23.0 + elapsed_time_.toSec())));
+  double w_4 = amplitude * (std::sin(1.25 * 2 * M_PI * (-28.0 + elapsed_time_.toSec())));
+  double w_5 = amplitude * (std::sin(2.00 * 2 * M_PI * (-32.8 + elapsed_time_.toSec())));
+  double w_6 = amplitude * (std::sin(2.50 * 2 * M_PI * (-37.8 + elapsed_time_.toSec())));
+  double w_7 = amplitude * (std::sin(3.20 * 2 * M_PI * (-42.6 + elapsed_time_.toSec())));
+
+  double w_1_2 = amplitude * (std::sin(0.20 * 2 * M_PI * (elapsed_time_.toSec())));
+  double w_2_2 = amplitude * (std::sin(0.50 * 2 * M_PI * (-15.0 - 42.6 + elapsed_time_.toSec())));
+  double w_3_2 = amplitude * (std::sin(1.00 * 2 * M_PI * (-23.0 - 42.6 + elapsed_time_.toSec())));
+  double w_4_2 = amplitude * (std::sin(1.25 * 2 * M_PI * (-28.0 - 42.6 + elapsed_time_.toSec())));
+  double w_5_2 = amplitude * (std::sin(2.00 * 2 * M_PI * (-32.8 - 42.6 + elapsed_time_.toSec())));
+  double w_6_2 = amplitude * (std::sin(2.50 * 2 * M_PI * (-37.8 - 42.6 + elapsed_time_.toSec())));
+  double w_7_2 = amplitude * (std::sin(3.20 * 2 * M_PI * (-42.6 - 42.6 + elapsed_time_.toSec())));
+
+  double w_1_3 = amplitude * (std::sin(0.20 * 2 * M_PI * (elapsed_time_.toSec())));
+  double w_2_3 = amplitude * (std::sin(0.50 * 2 * M_PI * (-15.0 - 42.6 - 42.6 + elapsed_time_.toSec())));
+  double w_3_3 = amplitude * (std::sin(1.00 * 2 * M_PI * (-23.0 - 42.6 - 42.6 + elapsed_time_.toSec())));
+  double w_4_3 = amplitude * (std::sin(1.25 * 2 * M_PI * (-28.0 - 42.6 - 42.6 + elapsed_time_.toSec())));
+  double w_5_3 = amplitude * (std::sin(2.00 * 2 * M_PI * (-32.8 - 42.6 - 42.6 + elapsed_time_.toSec())));
+  double w_6_3 = amplitude * (std::sin(2.50 * 2 * M_PI * (-37.8 - 42.6 - 42.6 + elapsed_time_.toSec())));
+  double w_7_3 = amplitude * (std::sin(3.20 * 2 * M_PI * (-42.6 - 42.6 - 42.6 + elapsed_time_.toSec())));
+
   if (elapsed_time_.toSec() <= 15){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_1);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 4) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_1);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 23){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_2);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 4) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_2);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 28){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_3);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 4) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_3);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 32.8){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_4);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 4) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_4);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 37.8){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_5);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 4) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_5);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 42.6){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_6);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 4) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_6);if (elapsed_time_.toSec() <= 15){
     ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_1);
     for (size_t i = 0; i < 7; ++i) {
       if (i == 4) {
@@ -280,7 +332,134 @@ void JointPositionExampleController::update(const ros::Time& /*time*/, // Update
     }
     elapsed_time_ = elapsed_time_ - duration;
   }
-  
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 15+42.6){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_1_2);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 5) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_1_2);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 23+42.6){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_2_2);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 5) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_2_2);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 28+42.6){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_3_2);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 5) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_3_2);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 32.8+42.6){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_4_2);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 5) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_4_2);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 37.8+42.6){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_5_2);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 5) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_5_2);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 42.6+42.6){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_6_2);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 5) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_6_2);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 15+42.6*2){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_1_3);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 6) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_1_3);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 23+42.6*2){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_2_3);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 6) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_2_3);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 28+42.6*2){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_3_3);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 6) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_3_3);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 32.8+42.6*2){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_4_3);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 6) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_4_3);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 37.8+42.6*2){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_5_3);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 6) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_5_3);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() <= 42.6+42.6*2){
+    ROS_ERROR("Time: %f, Delta Angle: %f", elapsed_time_.toSec(), w_6_3);
+    for (size_t i = 0; i < 7; ++i) {
+      if (i == 6) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + w_6_3);
+      } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+      }
+    }
+  } else if (elapsed_time_.toSec() < (42.6*3+5*60))
+  {
+    ROS_ERROR("Time: %f", elapsed_time_.toSec());
+    for (size_t i = 0; i < 7; ++i) {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+    }
+  }
+  else if (elapsed_time_.toSec() == (42.6*3+5*60))
+  {
+    ROS_ERROR("Time: %f", elapsed_time_.toSec());
+    for (size_t i = 0; i < 7; ++i) {
+        position_joint_handles_[i].setCommand(initial_pose_[i]);
+    }
+    elapsed_time_ = elapsed_time_ - duration;
+  }
+
 }
 
 }  // namespace franka_example_controllers
